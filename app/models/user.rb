f7c23aec849bf253715f8031bfa3befe9072f37e
Saushthav Saxena <saxenasaushthav@gmail.com>
2020-08-23 20:52:01 -0400

    added user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email,
+    format: { :with => /[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+/, message: 'Invalid Email' },
+    uniqueness: { case_sensitive: false }, length: { maximum: 254 }
+  validates_presence_of :first_name, :password
+end
