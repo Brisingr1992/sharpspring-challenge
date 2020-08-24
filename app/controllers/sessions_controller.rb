13aae526690aa1be7ba5f030a4accdf4aa1c85de
Saushthav Saxena <saxenasaushthav@gmail.com>
2020-08-23 21:19:08 -0400

    added views, sessions and simplecov

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,17 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to notes_path, notice: "Logged in!"
+    else
+      redirect_to new_session_path, alert: "Email or password is invalid"
+    end
   end
   
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged out!"
   end
 end
